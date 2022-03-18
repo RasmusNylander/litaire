fd4b0f1da63bcb7ddfc414b56abdae04798d968a
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2022-03-18 12:40:28 +0100

    SolitaireSolver: Move classes klondike::Column and klondike::Foundation
    This moves classes klondike::Column and klondike::Foundation into their
    own files, as they have now grown to contain some implementation. This
    greatly increases readability, and reduces confusion when working on the
    code.

---
+++ SolitaireSolver/src/main/java/model/klondike/Foundation.java
@@ -0,0 +1,35 @@
+package model.klondike;
+
+import model.Card;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Optional;
+import java.util.Stack;
+
+class Foundation extends Stack<Integer> {
+	protected Foundation() {
+		super();
+	}
+
+	@Override
+	public Integer push(@NotNull Integer card) {
+		if (!canAcceptCard(card))
+			throw new IllegalArgumentException("Error: Cannot push card: [" + card + "] onto Foundation: " + this);
+		return card;
+	}
+
+	@Contract(pure = true)
+	public boolean canAcceptCard(@NotNull Integer card) {
+		if (card == Card.Unknown) return false;
+		if (isEmpty()) return (card & Card.RankMask) == Card.Ace;
+		return card - peek() == 1;
+	}
+
+	@Contract(pure = true)
+	public Optional<Integer> asDestination() {
+		if (isEmpty())
+			return Optional.empty();
+		return Optional.of(peek());
+	}
+}
--- SolitaireSolver/src/main/java/model/klondike/Klondike.java
+++ SolitaireSolver/src/main/java/model/klondike/Klondike.java
@@ -3,68 +3,9 @@ package model.klondike;
 import model.Card;
 import model.Move;
 import model.Solitaire;
-import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
-
-class Foundation extends Stack<Integer> {
-	protected Foundation() {
-		super();
-	}
-
-	@Override
-	public Integer push(@NotNull Integer card) {
-		if (!canAcceptCard(card))
-			throw new IllegalArgumentException("Error: Cannot push card: [" + card + "] onto Foundation: " + this);
-		return card;
-	}
-
-	@Contract(pure = true)
-	public boolean canAcceptCard(@NotNull Integer card) {
-		if (card == Card.Unknown) return false;
-		if (isEmpty()) return (card & Card.RankMask) == Card.Ace;
-		return card - peek() == 1;
-	}
-
-	@Contract(pure = true)
-	public Optional<Integer> asDestination() {
-		if (isEmpty())
-			return Optional.empty();
-		return Optional.of(peek());
-	}
-}
-
-class Column extends Vector<Integer> {
-	protected Column(int numberOfCards) {
-		super(numberOfCards);
-		for (int i = 0; i < numberOfCards; i++) {
-			this.add(Card.Unknown);
-		}
-	}
-
-	public void reveal(@NotNull Integer card, int index) {
-		if (elementAt(index) != Card.Unknown)
-			throw new IllegalArgumentException("Error: Card at index " + index + " is already revealed!");
-		this.setElementAt(card, index);
-	}
-
-	@Contract(pure = true)
-	public Optional<Integer> asDestination() {
-		if (isEmpty())
-			return Optional.empty();
-		if (lastElement() == Card.Unknown)
-			throw new IllegalStateException("Error: Undefined! Unknown card cannot be a destination.");
-		return Optional.of(lastElement());
-	}
-
-	@Contract(pure = true)
-	public boolean canAcceptCard(@NotNull Integer card) {
-		if (isEmpty()) return card == Card.King;
-		if ((card & Card.Colour) == (lastElement() & Card.Colour)) return false;
-		return (lastElement() & Card.RankMask) - (card & Card.RankMask) == 1;
-	}
-}
 
 public record Klondike(@NotNull Foundation[] foundations, @NotNull Column[] columns,
                        @NotNull Stock stock) implements Solitaire {
@@ -109,7 +50,6 @@ public record Klondike(@NotNull Foundation[] foundations, @NotNull Column[] colu
 				break;
 			}
 		}
-
 
 		for (Integer card : column) {
 			if ((card & Card.RankMask) == Card.Unknown)
