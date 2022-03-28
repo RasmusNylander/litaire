7cd1a7a9069f16d674f5da529a58a455b9068be3
Rasmus Nylander <43042651+RasmusNylander@users.noreply.github.com>
2022-03-28 20:53:29 +0200

    SolitaireSolver: Make klondike::Foundation implement CardContainer
    This makes klondike::Foundation implement CardContainer adding the
    methods klondike::Foundation::getNumberOfCards(),
    klondike::Foundation::reachableCards, klondike::Foundation::receive, and
    klondike::Foundation::move in the process.

--- SolitaireSolver/src/main/java/model/klondike/Foundation.java
+++ SolitaireSolver/src/main/java/model/klondike/Foundation.java
@@ -1,13 +1,15 @@
 package model.klondike;
 
 import model.Card;
+import model.IllegalMoveException;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Optional;
+import java.util.Set;
 import java.util.Stack;
 
-class Foundation extends Stack<Integer> {
+class Foundation extends Stack<Integer> implements CardContainer {
 	protected Foundation() {
 		super();
 	}
@@ -20,8 +22,8 @@ class Foundation extends Stack<Integer> {
 		return card;
 	}
 
-	@Contract(pure = true)
-	public boolean canAcceptCard(@NotNull Integer card) {
+	@Override
+	public boolean canAcceptCard(int card) {
 		if (card == Card.Unknown) return false;
 		if (isEmpty()) return (card & Card.RankMask) == Card.Ace;
 		return card - peek() == 1;
@@ -32,5 +34,35 @@ class Foundation extends Stack<Integer> {
 		if (isEmpty())
 			return Optional.empty();
 		return Optional.of(peek());
+	}
+
+	@Override
+	public void move(int card, @NotNull CardContainer destination) throws IllegalMoveException {
+		if (!this.reachableCards().contains(card))
+			throw new IllegalMoveException("Error: Cannot move card: [" + Card.asString(card) + "] from Foundation: " + this);
+		destination.receive(this.pop());
+	}
+
+	@Override
+	public void receive(int... cards) throws IllegalMoveException {
+		for (int i = 0; i < cards.length; i++) {
+			int card = cards[i];
+			if (!canAcceptCard(card)) {
+				for (int j = 0; j < i; j++) this.pop(); // Restore the original state
+				throw new IllegalMoveException("Error: Cannot push card: [" + Card.asString(card) + "] onto Foundation: " + this);
+			}
+			push(card);
+		}
+	}
+
+	@Override
+	public @NotNull Set<Integer> reachableCards() {
+		if (isEmpty()) return Set.of();
+		return Set.of(this.peek());
+	}
+
+	@Override
+	public int getNumberOfCards() {
+		return size();
 	}
 }
--- SolitaireSolver/src/test/java/model/klondike/FoundationTest.java
+++ SolitaireSolver/src/test/java/model/klondike/FoundationTest.java
@@ -1,8 +1,10 @@
 package model.klondike;
 
 import model.Card;
+import model.IllegalMoveException;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -68,6 +70,87 @@ class FoundationTest {
 		Foundation foundation = new Foundation();
 		foundation.addElement(Card.Six | Card.Colour);
 		assertEquals(Optional.of(Card.Six | Card.Colour), foundation.asDestination());
+	}
+
+
+	@Test
+	void reachable_cards_should_be_not_be_null() {
+		assertNotNull(new Foundation().reachableCards());
+	}
+
+	@Test
+	void reachable_cards_should_be_empty_when_empty() {
+		assertTrue(new Foundation().reachableCards().isEmpty());
+	}
+
+	@Test
+	void reachable_cards_should_not_be_empty_when_empty() {
+		Foundation foundation = new Foundation();
+		foundation.addElement(Card.Ace | Card.Colour);
+		assertFalse(foundation.reachableCards().isEmpty());
+	}
+
+	@Test
+	void reachable_cards_should_only_be_size_one() {
+		Foundation foundation = new Foundation();
+		foundation.addAll(List.of(Card.Ace | Card.Colour, Card.Two | Card.Colour, Card.Three | Card.Colour));
+		assertEquals(1, foundation.reachableCards().size());
+	}
+
+	@Test
+	void reachable_cards_should_contain_last_card() {
+		Foundation foundation = new Foundation();
+		foundation.addAll(List.of(Card.Ace | Card.Colour, Card.Two | Card.Colour, Card.Three | Card.Colour));
+		assertTrue(foundation.reachableCards().contains(Card.Three | Card.Colour));
+	}
+
+	@Test
+	void receive_cards_should_throw_exception_when_cannot_accept_card() {
+		Foundation foundation = new Foundation();
+		assertThrows(IllegalMoveException.class, () -> new Foundation().receive(Card.Unknown), "Cannot receive Unknown card");
+		foundation.addElement(Card.Five | Card.Colour);
+		assertThrows(IllegalMoveException.class, () -> new Foundation().receive((Card.Six | Card.Type)));
+	}
+
+	@Test
+	void receive_cards_should_add_card_to_foundation() {
+		Foundation foundation = new Foundation();
+		foundation.receive(Card.Ace | Card.Colour);
+		assertEquals(1, foundation.size());
+	}
+
+	@Test
+	void state_should_be_unchanged_when_cannot_receive_card() {
+		Foundation foundation = new Foundation(), original = new Foundation();
+		original.addAll(List.of(Card.Ace, Card.Two, Card.Three));
+		foundation.addAll(List.of(Card.Ace, Card.Two, Card.Three));
+		assertThrows(Exception.class, () -> foundation.receive(Card.Four, Card.Five, Card.King), "Assumption: Cannot receive cards, and should thus throw exception.\nError: No exception thrown.");
+		assertEquals(original, foundation);
+	}
+
+	@Test
+	void move_should_throw_exception_if_card_is_not_reachable() {
+		Foundation foundation = new Foundation();
+		foundation.addAll(List.of(Card.Ace, Card.Two, Card.Three));
+		assertThrows(IllegalMoveException.class, () -> foundation.move(Card.Two, new MockCardContainer()));
+	}
+
+	@Test
+	void move_should_call_receive_on_destination_with_moved_card() {
+		Foundation foundation = new Foundation();
+		foundation.addAll(List.of(Card.Ace, Card.Two, Card.Three));
+		MockCardContainer destination = new MockCardContainer();
+		foundation.move(Card.Three, destination);
+		assertTrue(destination.receivedWasCalled);
+	}
+
+	@Test
+	void move_should_modify_foundation() {
+		Foundation foundation = new Foundation(), original = new Foundation();
+		foundation.addAll(List.of(Card.Ace, Card.Two, Card.Three));
+		original.addAll(List.of(Card.Ace, Card.Two, Card.Three));
+		foundation.move(Card.Three, new MockCardContainer());
+		assertNotEquals(original, foundation);
 	}
 
 }
