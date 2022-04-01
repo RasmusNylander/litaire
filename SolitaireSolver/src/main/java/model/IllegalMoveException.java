56e9cfa5bde017b67f7125e386a7f6199cb73a8b
Rasmus Nylander <43042651+RasmusNylander@users.noreply.github.com>
2022-04-01 17:45:47 +0200

    SolitaireSolver: Remove unused constructor of IllegalMoveException
    This simply removes the unintended and unused default constructor
    of klondike::IllegalMoveException.

--- SolitaireSolver/src/main/java/model/IllegalMoveException.java
+++ SolitaireSolver/src/main/java/model/IllegalMoveException.java
@@ -1,10 +1,6 @@
 package model;
 
 public class IllegalMoveException extends IllegalArgumentException {
-	public IllegalMoveException() {
-		super();
-	}
-
 	public IllegalMoveException(String message) {
 		super(message);
 	}
