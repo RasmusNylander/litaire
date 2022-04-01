b867d9dcfb01221cba3c517ef43c6583bff49975
Rasmus Nylander <43042651+RasmusNylander@users.noreply.github.com>
2022-04-01 19:24:15 +0200

    SolitaireSolver: Make klondike::Stock public
    This makes klondike::Stock public, as a klondike::Stock is necessary
    for the creation of an object of the public type klondike::Klondike.

--- SolitaireSolver/src/main/java/model/klondike/Stock.java
+++ SolitaireSolver/src/main/java/model/klondike/Stock.java
@@ -7,7 +7,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
 
-class Stock implements CardContainer {
+public class Stock implements CardContainer {
 	private final int[] cards;
 	private int size;
 	private int waste;
